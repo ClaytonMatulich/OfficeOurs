53592b7d699271dfee1243cb733de493506cab58
Unknown <claytonmatulich13@gmail.com>
2018-04-21 16:15:41 -0700

    Added Registration View Controller

---
+++ OfficeOurs/OfficeOurs/RegistrationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationViewController.swift
+//  OfficeOurs
+//
+//  Created by Clayton Matulich on 4/21/18.
+//  Copyright © 2018 Clayton Matulich. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
