4178a62d6da7f44991adf599f3992a4697797b77
Unknown <claytonmatulich13@gmail.com>
2018-04-21 16:54:04 -0700

    Registration Page

--- OfficeOurs/OfficeOurs/RegistrationViewController.swift
+++ OfficeOurs/OfficeOurs/RegistrationViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 class RegistrationViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +29,18 @@ class RegistrationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func registrationPressed(_ sender: UIButton) {
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil{
+                print(error!)
+            }else{
+                print("Registration Successfull")
+                //self.performSegue(withIdentifier: "", sender: self)
+            }
+        }
     }
-    */
+    
+   
 
 }
