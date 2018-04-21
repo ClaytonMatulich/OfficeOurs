2c2ce4a97ef5b7110dbe03be73d95ca836636d16
Unknown <claytonmatulich13@gmail.com>
2018-04-21 12:47:43 -0700

    Uploaded Base Project

---
+++ OfficeOurs/OfficeOurs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfficeOurs
+//
+//  Created by Clayton Matulich on 4/21/18.
+//  Copyright © 2018 Clayton Matulich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
