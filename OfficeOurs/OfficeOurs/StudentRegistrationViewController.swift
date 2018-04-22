6fb82f35269584e222080950fcd72887a870434b
Unknown <claytonmatulich13@gmail.com>
2018-04-21 22:03:30 -0700

    Gave back buttons to the student and teacher registration pages

--- OfficeOurs/OfficeOurs/StudentRegistrationViewController.swift
+++ OfficeOurs/OfficeOurs/StudentRegistrationViewController.swift
@@ -22,14 +22,9 @@ class StudentRegistrationViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
--- OfficeOurs/OfficeOurs/TeacherRegistrationViewController.swift
+++ OfficeOurs/OfficeOurs/TeacherRegistrationViewController.swift
@@ -21,15 +21,9 @@ class TeacherRegistrationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    
 }
