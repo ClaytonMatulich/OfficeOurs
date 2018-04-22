06c50e66e059f5e0678549994dba57a962862ca2
Unknown <claytonmatulich13@gmail.com>
2018-04-21 17:33:56 -0700

    Added ability to login

--- OfficeOurs/OfficeOurs/ViewController.swift
+++ OfficeOurs/OfficeOurs/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 import ChameleonFramework
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     
     override func viewDidLoad() {
@@ -24,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressedLoginButton(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil{
+                print(error!)
+            }else{
+                print("Login Successfull")
+                //self.performSegue(withIdentifier: "", sender: self)
+            }
+        }
+    }
+    
 }
 
